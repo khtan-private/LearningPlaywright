4fe2d1afd1a30c40311debcabb624d24e78ef9c0
KHT@air <tan.k.h@juno.com>
2023-02-14 17:19:38 -0800

    update

--- ts/reproProblem/x.bat
+++ ts/reproProblem/x.bat
@@ -1,4 +1,12 @@
 @echo on
+echo "----------------"
+echo "--- Tests in pw4"
+echo "----------------"
 set TT=C:\cprojects\github\khtan-private\LearningPlaywright\ts\reproProblem\pw4
-docker run --volume %TT%:/tests -it --rm --ipc=host pw4
+docker run --volume %TT%:/tests --rm --ipc=host pw4:v2
 
+echo "---------------------"
+echo "--- Tests in pw4tests"
+echo "---------------------"
+set TT=C:\cprojects\github\khtan-private\LearningPlaywright\ts\reproProblem\pw4tests
+docker run --volume %TT%:/tests --rm --ipc=host pw4:v2
